51ecfe5091eafe3742a7ee06c6f272a22d736bdb
Adrian Messado <adrian.messado@gmail.com>
2018-04-20 13:00:50 -0700

    create method to convert years into seconds, test that function is working in spec

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -5,12 +5,19 @@ describe('AgeConvertor', function() {
   beforeEach(function() {
 
   });
+  it('Return age in seconds ', function() {
+    let age = 27
+    let newAge = new AgeConvertor(age);
+
+    newAge.ageSec()
+    console.log(newAge.ageSec())
+    expect(newAge.ageSec()).toEqual(851472000);
+  });
   it('Return age in Mercury years ', function() {
     let age = 27
     let newAge = new AgeConvertor(age);
 
     newAge.mercuryCalc()
-    console.log(newAge.age)
     console.log(newAge.mercuryCalc())
     expect(newAge.mercuryCalc()).toEqual(112.5);
   });
--- src/AgeConvertor.js
+++ src/AgeConvertor.js
@@ -2,8 +2,11 @@ class AgeConvertor {
   constructor(age) {
     this.age = age;
     this.mercury = .24;
+    this.earthYearInSec = 31536000;
   }
-
+  ageSec(){
+    return (this.age * this.earthYearInSec);
+  }
   mercuryCalc() {
     return (this.age / .24);
   }
